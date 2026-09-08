dcf46b16ceaf449e6148fb26857a24ce0fa90db8
FradjF <fradj.ferchichi@gmail.com>
2026-09-08 23:16:29 +0200

    Improve CLI configuration error handling

--- proj01_smartFileOrganizer/organizer/main.py
+++ proj01_smartFileOrganizer/organizer/main.py
@@ -9,26 +9,39 @@ import logging
 logger = logging.getLogger(__name__)
 #config_path = Path(__file__).parent / "config.json"
 
-def main() -> None:
+def main() -> int:
     configure_logging()
+    #Parse arguments
     args = parse_argument()
+
+    #Validate folder
     source = validate_path(args.folder_path)
-    config = args.config
     if source is None:
         raise FileNotFoundError("Folder has not been found.")
 
-    dry_run = args.dry_run
-    if config is None:
+    #Load configuration
+    config = args.config
+    if not config is None:
+        try:
+            categories = get_configuration(Path(config))
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
+            return 1
+        except ValueError as e:
+            print(f"Error: {e}")
+            return 1
+    else:
         categories = DEFAULT_CATEGORIES
-    else:
-        categories = get_configuration(Path(config))
 
-    if not validate_configuration(categories):
-        logger.error("Configuration file has the wrong format.")
-        raise ValueError("Invalid configuration.")
-    else:
+    #Validate configuration
+    if validate_configuration(categories):
         logger.info("Configuration file is valid.")
+        dry_run = args.dry_run
         organize_folder(source, categories, dry_run)
+    else:
+        logger.error("Configuration file has the wrong format.")
+        print("Error: Config file has the wrong format.")
+        return 1
 
 if __name__ == "__main__":
     main()
